f0f13a275ca7b41b562fb549519f16111469a5fa
Jonathas Cavalcante <jonathas.cavalcante@outlook.com.br>
2023-07-17 23:44:41 -0300

    Updated AppContext.tsx

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -13,15 +13,15 @@ export const AppContext = createContext({} as IAppContext)
 
 export const AppContextProvider = ({ children }: any) => {
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+  
   const storage = getAllLocalStorage()
   
   useEffect(()=>{
     if(storage){
       const { login } = JSON.parse(storage)
       setIsLoggedIn(login)
-      createLocalStorage()
     }  
-  })
+  }, [])
   
   const [title, setTitle] = useState<string>('')
   const user = 'Jon'
